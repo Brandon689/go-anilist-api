1129a726c2bbe79bff3f4f66357b2fa4517a0795
Brandon <brandon139@protonmail.com>
2024-03-18 00:50:52 +1100

    Initial commit

---
+++ anilist.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/rl404/verniy"
+)
+
+// Handle *string to string.
+func ptrToString(str *string) string {
+	if str == nil {
+		return ""
+	}
+	return *str
+}
+
+func main() {
+	v := verniy.New()
+
+	// Get anime One Piece data (with default fields).
+
+	// d, err := c.SearchAnime(verniy.PageParamMedia{
+	// 	Season:     verniy.SeasonWinter,
+	// 	SeasonYear: 2021,
+	// 	TagIn: []string{"Action"},
+	// }, 1, 10)
+	data, err := v.SearchAnime(verniy.PageParamMedia{
+		Search: "Yu Yu Hakusho",
+	}, 1, 50)
+	//data, err := v.GetAnime(21)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Convert the data object to JSON.
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		fmt.Println("Error marshalling to JSON:", err)
+		return
+	}
+
+	// Print the JSON data.
+	fmt.Println("Anime Data as JSON:")
+	fmt.Println(string(jsonData))
+
+	// Access specific fields using the pointer handler.
+	// fmt.Println("\nAnime Data Fields:")
+	// fmt.Printf("Title (Romaji): %s\n", ptrToString(data.Title.Romaji))
+	// fmt.Printf("Title (English): %s\n", ptrToString(data.Title.English))
+	// fmt.Printf("Title (Native): %s\n", ptrToString(data.Title.Native))
+}
